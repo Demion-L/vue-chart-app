8d3c7889b9ef110bf432e9e6bb27aa390708e757
Demion-L <padovds@ukr.net>
2024-07-15 20:31:25 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div>
+      <highcharts-component :chartOptions="chartOptions" chartId="myChart" />
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HighchartsComponent from "./components/HighchartsComponent.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { HighchartsComponent },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: "bar",
+        },
+        title: {
+          text: "Fruit Consumption",
+        },
+        xAxis: {
+          categories: ["Apples", "Bananas", "Oranges"],
+        },
+        yAxis: {
+          title: {
+            text: "Fruit eaten",
+          },
+        },
+        series: [
+          {
+            name: "Jane",
+            data: [1, 0, 4],
+          },
+          {
+            name: "John",
+            data: [5, 7, 3],
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/HighchartsComponent.vue
@@ -0,0 +1,29 @@
+vueCopy
+<template>
+  <div :id="chartId"></div>
+</template>
+
+<script>
+import Highcharts from "highcharts";
+
+export default {
+  props: {
+    chartOptions: {
+      type: Object,
+      required: true,
+    },
+    chartId: {
+      type: String,
+      default: "highcharts",
+    },
+  },
+  mounted() {
+    this.chart = Highcharts.chart(this.chartId, this.chartOptions);
+  },
+  beforeDestroy() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+  },
+};
+</script>
